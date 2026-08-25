884d01c550d92d85525a3bc1ecab7bc548e5d12c
osiute <osiute.programming@gmail.com>
2026-08-25 22:43:35 +0300

    load.lisp для автоматизирования подключения проекта к sbcl

---
+++ load.lisp
@@ -0,0 +1,22 @@
+;; Автоматическое подключение библиотеки и тестов (чтобы каждый раз не прописывать подключение библиотек в sbcl вручную)
+(require :asdf)
+
+(push #p"./" asdf:*central-registry*) ; Включение текущей директории в диапазон поиска проектов для asdf
+
+(format t "~%[1/3] Загрузка основной системы...~%")
+(asdf:load-system :regex-library)
+
+(format t "~%[2/3] Загрузка тестовой системы...~%")
+(asdf:load-system :regex-library/tests)
+
+(format t "~%[3/3] Переход в пакет...~%")
+(in-package :regex-library)
+
+(format t "~%=== Проект успешно загружен! ===~%")
+(format t "Доступные тесты:~%")
+(format t "(run-parser-tests)~%")
+(format t "(run-unicode-tests)~%")
+(format t "(run-nfa-tests)~%")
+(format t "(run-dfa-tests)~%")
+(format t "(run-engine-tests)~%")
+(format t "~%")
