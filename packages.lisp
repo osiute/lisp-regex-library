cbd213e60b3a2daf2aa001ebc88bb6c92a794b0a
osiute <osiute.programming@gmail.com>
2026-09-10 14:04:03 +0300

    Окончательно определён экспортируемый функционал библиотеки

--- packages.lisp
+++ packages.lisp
@@ -7,11 +7,10 @@
     #:compile-regex
     #:regex-match-p
     #:regex-search-p
-    #:regex-find-first
-    #:regex-find-last
-    #:regex-find-all
+    #:regex-find-first-match-bounds ; возвращает пару (start . end) для первого совпадения или NIL, если совпадений нет.
+    #:make-regex-match-bounds-iterator ; возвращает итератор по всем совпадениям, который при каждом вызове возвращает пару (start . end) для очередного совпадения или NIL, если совпадений больше нет.
+    #:regex-match-string ; по переданным (start . end) возвращает подстроку исходного текста, которая соответствует совпадению.
     #:regex-split
     #:regex-replace-all
-    #:run-tests
   )
 )
